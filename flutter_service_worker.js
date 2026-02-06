da72e502a69ff3de7ebdbb403a66800c641b8823
Ronald Gao <ronald.s.gao@gmail.com>
2026-02-05 21:02:12 -0800

    test try this demo text

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9ec6a0a41398a6862f27bbf46ddb3e64",
+const RESOURCES = {"flutter_bootstrap.js": "98a21ed5358dddd9a52a781bd6696da2",
 "version.json": "6f43113597417d626c4e4b2b93e36c35",
 "index.html": "d8dba7d81ff06ee8aeab6fb44fe1fbf2",
 "/": "d8dba7d81ff06ee8aeab6fb44fe1fbf2",
-"main.dart.js": "e96bf2dffca68555e13546d154aa1f3a",
+"main.dart.js": "8f1505817167dbf324af0991ec34d645",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "icons/Icon-192.png": "850659a823c7f28d5bd2709f5a9da41d",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
