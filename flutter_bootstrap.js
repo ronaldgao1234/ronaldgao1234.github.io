0a8c680893a590978c454ada33715f43069977cc
Ronald Gao <ronald.s.gao@gmail.com>
2026-05-25 00:50:26 -0700

    change what it says in google searches

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "235239960" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4049279780" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
